5e1f5de2c013053238a4fc9294f44b84c6c6688a
Qiyuan Pu <pqy7172@gmail.com>
2018-02-11 19:29:09 +0800

    solved 1004, 13 lines

---
+++ 1004.c
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("PROD = %d\n", a * b);
+    
+    return 0;
+}
+
