1a03ffc8fa0e86e9bb0efe7f558fd9ea10a85fde
Qiyuan Pu <pqy7172@gmail.com>
2018-02-11 17:10:47 +0800

    solved 1002

---
+++ 1002.c
@@ -0,0 +1,12 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	double r, x;
+	scanf("%lf", &r);
+	printf("A=%.4f\n", 3.14159 * r * r);
+    
+    return 0;
+}
+
