40e7ef61574e971ceda9f9be2f82daf31c3d07b2
Qiyuan Pu <pqy7172@gmail.com>
2018-02-11 20:04:19 +0800

    solved 1006, 13 lines

---
+++ 1006.c
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1006
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	float a, b, c;
+	scanf("%f %f %f", &a, &b, &c);
+	printf("MEDIA = %.1f\n", a * 0.2 + b * 0.3 + c * 0.5);
+    
+    return 0;
+}
+
