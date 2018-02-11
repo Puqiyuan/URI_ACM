8a3bd9ee2c3d6b8647af1ef4cf453cb6f54f0ae4
Qiyuan Pu <pqy7172@gmail.com>
2018-02-11 19:23:24 +0800

    solved 1003, 12

---
+++ 1003.c
@@ -0,0 +1,12 @@
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("SOMA = %d\n", a + b);
+	
+    return 0;
+}
