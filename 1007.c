6fee01cb4bce45be0e15007ac4c050f0a26317d9
Qiyuan Pu <pqy7172@gmail.com>
2018-02-11 20:11:51 +0800

    solved 1007, 13 lines

---
+++ 1007.c
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("DIFERENCA = %d\n", a * b - c * d);
+    
+    return 0;
+}
+
