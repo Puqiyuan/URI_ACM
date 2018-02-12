a52d649b6c648ba92500751a7234c1a97e1925ea
Qiyuan Pu <pqy7172@gmail.com>
2018-02-12 10:16:11 +0800

    solved 1011, 13 lines

---
+++ 1011.c
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1011
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	double r;
+	scanf("%lf", &r);
+	printf("VOLUME = %.3f\n", (4.0/3) * 3.14159 * r * r *r);
+	
+    return 0;
+}
+
