501a2ae809bab8c0ec456528fc1ab62635ed8136
Qiyuan Pu <pqy7172@gmail.com>
2018-02-12 16:17:23 +0800

    solved 1016, 13 lines

---
+++ 1016.c
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1016
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int dis;
+	scanf("%d", &dis);
+	printf("%d minutos\n", dis * 2);
+	
+    return 0;
+}
+
