e534fa5c55d4d8107d2eca3b82627a4520146607
Qiyuan Pu <pqy7172@gmail.com>
2018-03-04 22:12:03 +0800

    solved 1044, 18 lines

---
+++ 1044.c
@@ -0,0 +1,18 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1044
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if (a % b == 0 || b % a ==0)
+		printf("Sao Multiplos\n");
+
+	else
+		printf("Nao sao Multiplos\n");
+    
+    return 0;
+}
+
