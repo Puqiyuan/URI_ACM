269b5d1546d5301b1ae4c98d992189fe301f8660
Qiyuan Pu <pqy7172@gmail.com>
2018-05-05 11:27:26 +0800

    empty

--- 1007.c
+++ 1007.c
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[])
 {
     int a, b, c, d;
-	scanf("%d %d %d %d", &a, &b, &c, &d);
-	printf("DIFERENCA = %d\n", a * b - c * d);
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("DIFERENCA = %d\n", a * b - c * d);
     
     return 0;
 }
