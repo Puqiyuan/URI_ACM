88d36cd867430df845aeea5acf73d35325bac7f3
Qiyuan Pu <pqy7172@gmail.com>
2018-05-05 22:41:52 +0800

    solved 1067 problem, 18 lines

---
+++ 1067.c
@@ -0,0 +1,18 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1067
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int x, i;
+	scanf("%d", &x);
+	
+	for (i = 1; i <= x; i++)
+		{
+			if (i % 2 != 0)
+				printf("%d\n", i);
+		}
+	
+    return 0;
+}
+
