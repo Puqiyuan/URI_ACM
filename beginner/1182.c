48718f27edaf2e3202281a9e95314193337b6378
Qiyuan Pu <pqy7172@gmail.com>
2018-06-05 22:36:07 +0800

    solved 1182, 31 lines

---
+++ beginner/1182.c
@@ -0,0 +1,31 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1181
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	float m[12][12], sum = 0.0;
+	int i, j, l;
+	char t, eat;
+
+	scanf("%d", &l);
+	scanf("%c", &eat);
+	scanf("%c", &t);
+	scanf("%c", &eat);
+
+	for (i = 0; i <= 11; i++)
+		{
+			for (j = 0; j <= 11; j++)
+				scanf("%f", &m[i][j]);
+		}
+
+	for (j = 0; j <= 11; j++)
+		sum += m[j][l];
+
+	if (t == 'S')
+		printf("%.1f\n", sum);
+	else
+	printf("%.1f\n", sum / 12.0);
+	
+    return 0;
+}
