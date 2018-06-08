344ae4ea6bc0c43b4a84d897e10928883534eeca
Qiyuan Pu <pqy7172@gmail.com>
2018-06-08 22:35:05 +0800

    solved 1557, 81 lines

--- beginner/1557.c
+++ beginner/1557.c
@@ -3,9 +3,50 @@
 #include <stdio.h>
 #include <math.h>
 
+int width(int data)
+{
+	if (data <= 9)
+		return 1;
+	else if (data <= 99)
+		return 2;
+	else if (data <= 999)
+		return 3;
+	else if (data <= 9999)
+		return 4;
+	else if (data <= 99999)
+		return 5;
+	else if (data <= 999999)
+		return 6;
+	else if (data <= 9999999)
+		return 7;
+	else if (data <= 99999999)
+		return 8;
+
+	return 9;
+}
+
+int space(int max, int length, int j)
+{
+	int i;
+
+	if (j == 0)
+		{
+			for (i = 0; i < max -length; i++)
+				printf(" ");
+		}
+
+	else
+		{
+			for (i = 0; i < max + 1 - length; i++)
+				printf(" ");
+		}
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	int n, i, j, width;
+	int n, i, j, max, length;
 	scanf("%d", &n);
 
 	while (n != 0)
@@ -19,18 +60,22 @@ int main(int argc, char *argv[])
 						data[i][j] = data[0][j] * pow(2, i);
 				}
 
+			max = width(data[n-1][n-1]);
+		
 			for (i = 0; i <= n - 1; i++)
 				{
-					for (j = 0; j <= n - 1;j ++)
-						printf("%d ", data[i][j]);
+					for (j = 0; j <= n - 1; j++)
+						{
+							length = width(data[i][j]);
+							space(max, length, j);
+							printf("%d", data[i][j]);
+						}
 					printf("\n");
 				}
 
-			if ()
-
-
+			printf("\n");
 			scanf("%d", &n);
 		}
-	
+
     return 0;
 }
