d2774e73181909fbeeee9cd9669c1d886075b16e
Qiyuan Pu <pqy7172@gmail.com>
2018-07-15 12:05:04 +0800

    solved of 1959

---
+++ beginner/1959.c
@@ -0,0 +1,14 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1959
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	long long int n, l, p;
+	scanf("%lld %lld", &n, &l);
+	p = n * l;
+	printf("%lld\n", n * l);
+	
+    return 0;
+}
+
