b44bfd0d1737653518c84aa81eebdfe8eb0e86be
Qiyuan Pu <pqy7172@gmail.com>
2018-08-12 11:49:45 +0800

    solved of 2146

---
+++ beginner/2146.c
@@ -0,0 +1,16 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/2146
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int p;
+
+	while (scanf("%d", &p) != EOF)
+	{
+		printf("%d\n", p - 1);
+	}
+	
+    return 0;
+}
+
