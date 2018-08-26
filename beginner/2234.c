6d3d443090bbf1c5dda91f619e0d59c03153073d
Qiyuan Pu <pqy7172@gmail.com>
2018-08-26 10:05:44 +0800

    accepted of 2234

---
+++ beginner/2234.c
@@ -0,0 +1,14 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/2234
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int h, p;
+	scanf("%d %d", &h, &p);
+
+	printf("%.2f\n", (h + 0.0) / p);
+	
+    return 0;
+}
+
