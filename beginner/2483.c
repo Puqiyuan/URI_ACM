126425e908a3be35ed07369895103823685b9daa
Qiyuan Pu <pqy7172@gmail.com>
2018-09-16 15:05:20 +0800

    accepted of 2483

---
+++ beginner/2483.c
@@ -0,0 +1,17 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/2483
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int n, i;
+	scanf("%d", &n);
+
+	printf("Feliz nat");
+	for (i = 0; i <= n - 1; i++)
+		printf("a");
+	printf("l!\n");
+	
+	return 0;
+}
+
